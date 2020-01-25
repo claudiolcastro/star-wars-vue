541e351cba507322197c32de0b2b4f43c0e27375
Claudio Castro <claudioluiz.castro@gmail.com>
2020-01-25 20:30:31 -0300

    create card component

---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="character-card">
+    <dir class="char-image">
+      <img src="" :alt="character.name">
+    </dir>
+
+    <span class="char-name">
+      {{ character.name }}
+    </span>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+
+import { fetchStarships } from '../repository/api';
+
+export default {
+  name: 'CharacterCard',
+
+  props: {
+    character: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  computed: {
+  },
+
+  methods: {
+    ...mapActions([
+    ]),
+  },
+
+  created() {
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .character-card {
+    border: 1px solid $black;
+    border-radius: 3px;
+    color: $white;
+    height: 285px;
+    width: 250px;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="home">
+    <img src="https://img.icons8.com/color/48/000000/lightsaber.png">
     <h1>Star Wars Characters</h1>
+    <img style="transform: rotate(270deg)" src="https://img.icons8.com/color/48/000000/lightsaber.png">
+
+    <ul class="characters-list">
+      <li v-for="(character, index) in characters" :key="index">
+        <CharacterCard :character="character" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex';
+
+import CharacterCard from '../components/CharacterCard.vue';
 
 import { fetchChatacters } from '../repository/api';
 
@@ -13,6 +23,7 @@ export default {
   name: 'home',
 
   components: {
+    CharacterCard,
   },
 
   computed: {
@@ -25,26 +36,44 @@ export default {
       'setPage',
     ]),
 
-    async loadCharacters() {
-      const { results } = await fetchChatacters(1);
+    async loadCharacters(page) {
+      const { results } = await fetchChatacters(page);
       this.setCharacters(results);
     },
   },
 
   created() {
-    this.loadCharacters();
+    this.loadCharacters(1);
   },
 };
 </script>
 
 <style lang="scss" scoped>
   .home {
-    margin-bottom: 30px;
+    background-color: #424242;
+    padding-bottom: 30px;
+    width: 100%;
 
     h1 {
+      color: $yellow;
+      display: inline-block;
       font-family: 'Bebas Neue', cursive;
-      font-size: 42px;
-      margin-top: 50px;
+      font-size: 52px;
+      margin: 50px 20px 0 20px;
+      @include media-mobile {
+        font-size: 32px;
+      }
+    }
+
+    .characters-list {
+      display: flex;
+      flex-wrap: wrap;
+      margin: 35px auto 0 auto;
+      width: fit-content;
+
+      li {
+        margin: 15px 0 0 15px;
+      }
     }
   }
 </style>
