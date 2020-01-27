b85b786efe858a43da6be4f91c30ca8b9022e195
Claudio Castro <claudioluiz.castro@gmail.com>
2020-01-27 18:34:00 -0300

    test charCard layout

--- tests/unit/components/CharacterCard.spec.js
+++ tests/unit/components/CharacterCard.spec.js
@@ -1,8 +1,37 @@
 import { shallowMount } from '@vue/test-utils';
 import CharacterCard from '../../../src/components/CharacterCard.vue';
 
-describe('CharacterCard.vue', () => {
-  it('Display Card with right informations', () => {
+describe('CharacterCard', () => {
+  it('Display Card with starships', () => {
+    const character = {
+      name: 'Luke Skywalker',
+      homeworld: 'https://swapi.co/api/planets/1/',
+      films: [
+        'https://swapi.co/api/films/2/',
+        'https://swapi.co/api/films/6/',
+      ],
+      starships: [
+        'https://swapi.co/api/starships/12/',
+        'https://swapi.co/api/starships/22/',
+      ],
+    };
+
+    const wrapper = shallowMount(CharacterCard, {
+      propsData: {
+        character,
+      },
+      data() {
+        return {
+          starships: [{ name: 'StarFighter', class: 'Fighter' }],
+        };
+      },
+    });
+
+    expect(wrapper.find('.char-name').text()).toBe('Luke Skywalker');
+    expect(wrapper.find('.char-starships').exists()).toBe(true);
+  });
+
+  it('Display Card with NO starships', () => {
     const character = {
       name: 'Luke Skywalker',
       homeworld: 'https://swapi.co/api/planets/1/',
@@ -22,7 +51,7 @@ describe('CharacterCard.vue', () => {
       },
     });
 
-    expect(wrapper.find('b').text()).toBe('Luke Skywalker');
-    // expect(wrapper.find('button').text()).toBe('submit');
+    expect(wrapper.find('.char-name').text()).toBe('Luke Skywalker');
+    expect(wrapper.find('.char-starships').exists()).toBe(false);
   });
 });
